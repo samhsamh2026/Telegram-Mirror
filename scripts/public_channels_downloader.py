2d3645c04474c4bed8e11f2099b1a005179e4633
Mig mig <hashem.1385.hashem@gmail.com>
2026-05-13 08:02:08 +0330

    Update public_channels_downloader.py

--- scripts/public_channels_downloader.py
+++ scripts/public_channels_downloader.py
@@ -9,8 +9,8 @@ from urllib.parse import urlparse
 
 import requests
 from bs4 import BeautifulSoup
-import shutil
-shutil.rmtree('channels')
+#import shutil
+#shutil.rmtree('channels')
 # ═══════════════════════════════════════════════════════════
 #  ثابت‌ها
 # ═══════════════════════════════════════════════════════════
